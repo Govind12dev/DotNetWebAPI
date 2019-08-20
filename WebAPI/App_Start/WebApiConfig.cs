ad21e89fbe541c8488900e3b2a47f04f8cf56e75
Govind <govind12btechit@gmail.com>
2019-08-20 04:31:37 -0700

    WebAPI: Added entity framework models

--- WebAPI/App_Start/WebApiConfig.cs
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPI
 {
@@ -9,6 +10,8 @@ namespace WebAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            //Enable CORS 
+            config.EnableCors(new EnableCorsAttribute("http://localhost:4200", headers: "*", methods: "*"));
             // Web API configuration and services
 
             // Web API routes
---
+++ WebAPI/Controllers/ProjectController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ProjectController : ApiController
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: api/Project
+        public IQueryable<Project> GetProjects()
+        {
+            return db.Projects;
+        }
+
+        // GET: api/Project/5
+        [ResponseType(typeof(Project))]
+        public IHttpActionResult GetProject(int id)
+        {
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+
+        // PUT: api/Project/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProject(int id, Project project)
+        {
+            if (id != project.ProjectID)
+            {
+                return BadRequest();
+            }
+
+            if (!ProjectExists(id))
+            {
+                return NotFound();
+            }
+
+            db.Entry(project).State = EntityState.Modified;
+
+            db.SaveChanges();
+
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Project
+        [ResponseType(typeof(Project))]
+        public IHttpActionResult PostProject(Project project)
+        {
+
+            db.Projects.Add(project);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = project.ProjectID }, project);
+        }
+
+        // DELETE: api/Project/5
+        /// <summary>
+        /// Delete Project
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(Project))]
+        public IHttpActionResult DeleteProject(int id)
+        {
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            db.Projects.Remove(project);
+            db.SaveChanges();
+
+            return Ok(project);
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return db.Projects.Count(e => e.ProjectID == id) > 0;
+        }
+    }
+}
