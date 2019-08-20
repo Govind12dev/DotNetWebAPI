0a08cc2e5cab26c6ec1fc30c3aae7fa6ed068b9a
Govind <govind12btechit@gmail.com>
2019-08-20 04:52:31 -0700

    WebAPI: Added controller

---
+++ WebAPI/Controllers/TaskController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class TaskController : ApiController
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: api/Task
+        public IQueryable<Task> GetTasks()
+        {
+            return db.Tasks;
+        }
+
+        // GET: api/Task/5
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult GetTask(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+        }
+
+        // PUT: api/Task/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTask(int id, Task task)
+        {
+            if (id != task.TaskID)
+            {
+                return BadRequest();
+            }
+            if (!TaskExists(id))
+            {
+                return NotFound();
+            }
+
+            db.Entry(task).State = EntityState.Modified;
+
+
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Task
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult PostTask(Task task)
+        {
+            db.Tasks.Add(task);
+
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = task.TaskID }, task);
+        }
+
+        // DELETE: api/Task/5
+        [ResponseType(typeof(Task))]
+        public IHttpActionResult DeleteTask(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+
+            return Ok(task);
+        }
+
+        private bool TaskExists(int id)
+        {
+            return db.Tasks.Count(e => e.TaskID == id) > 0;
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: api/User
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET: api/User/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/User/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser(int id, User user)
+        {
+            if (id != user.UserID)
+            {
+                return BadRequest();
+            }
+            if (!UserExists(id))
+            {
+                return NotFound();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+
+            db.SaveChanges();
+
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/User
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.UserID }, user);
+        }
+
+        // DELETE: api/User/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.Users.Count(e => e.UserID == id) > 0;
+        }
+    }
+}
